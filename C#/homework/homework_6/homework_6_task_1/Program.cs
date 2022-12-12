bbe260a5147539667378c57d2665c05cb5e2cd9d
Maxim Cherkasov <cherkasov.maxi@gmail.com>
2022-12-12 14:52:21 +0300

    Решил задачу 41 - ДЗ 6 C#

--- C#/homework/homework_6/homework_6_task_1/Program.cs
+++ C#/homework/homework_6/homework_6_task_1/Program.cs
@@ -1,3 +1,17 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
+
+string text = "0 7 8 -2 -2";
+
+int[] array = text.Split(' ').
+          Where(x => !string.IsNullOrWhiteSpace(x)).
+          Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine(count);
