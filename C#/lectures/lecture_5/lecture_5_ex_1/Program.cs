530f6d3ab1a1df954d00dbbd6dba32767aa6ebfa
Maxim Cherkasov <cherkasov.maxi@gmail.com>
2022-12-12 18:01:13 +0300

    lecture_5 C#

---
+++ C#/lectures/lecture_5/lecture_5_ex_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Как узнать тип переменной - использовать var
+
+using System.Linq;
+using System.Xml.Linq;
+
+var a = "la la la";
+Console.WriteLine(a.GetType());
+
+//var data = new int[] { 1, 2, 3, 4 }
+//    .Where(e => e > 0)
+//    .Select(e => new { q = e, w = e + 1 });
+//Console.WriteLine(data.GetType());
+
+// используйте индексы в циклах: i, j, k ,l , m;
+
+// не надо делать много функций в функциях;
